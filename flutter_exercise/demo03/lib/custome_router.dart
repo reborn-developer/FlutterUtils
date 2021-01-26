a43bad87d27b33b1b18e179de24b0223c100da6b
reborn <343633674@qq.com>
2021-01-26 19:06:22 +0800

    页面跳转4种路由动画

---
+++ flutter_exercise/demo03/lib/custome_router.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class CustomeRoute extends PageRouteBuilder {
+
+  final Widget widget;
+
+  CustomeRoute(this.widget)
+      :super(
+    transitionDuration: Duration(seconds: 1),
+    pageBuilder: (
+        BuildContext context,
+        Animation<double> animation1,
+        Animation<double> animation2
+        ) {
+      return widget;
+    },
+    transitionsBuilder: (
+        BuildContext context,
+        Animation<double> animation1,
+        Animation<double> animation2,
+        Widget child
+        ){
+      // 1、渐隐渐现的路由动画效果
+      // return FadeTransition(
+      //   opacity: Tween(begin: 0.0, end: 1.0)
+      //       .animate(CurvedAnimation(
+      //       parent: animation1,
+      //       curve: Curves.fastOutSlowIn
+      //   )),
+      //   child: child,
+      // );
+
+      // 2、缩放的路由动画效果
+      // return ScaleTransition(
+      //   scale: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+      //       parent: animation1,
+      //       curve: Curves.fastOutSlowIn
+      //   )),
+      //   child: child,
+      // );
+
+      // 3、旋转+缩放动画效果
+      // return RotationTransition(
+      //   turns: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+      //       parent: animation1,
+      //       curve: Curves.fastOutSlowIn
+      //   )),
+      //   child: ScaleTransition(
+      //     scale: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+      //       parent: animation1,
+      //       curve: Curves.fastOutSlowIn
+      //     )),
+      //     child: child,
+      //   ),
+      // );
+      
+      // 4、左右滑动动画
+      return SlideTransition(
+        position: Tween<Offset>(
+            begin: Offset(-1.0, 0.0),
+            end: Offset(0.0, 0.0)
+        ).animate(CurvedAnimation(
+            parent: animation1,
+            curve: Curves.fastOutSlowIn
+        )),
+        child: child,
+      );
+
+    },
+  );
+}
